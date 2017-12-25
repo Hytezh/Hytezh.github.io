62d910add08511ac563c98b2be5ced779974bfb4
Hytezh <34644531+Hytezh@users.noreply.github.com>
2017-12-25 18:18:16 +0530

    Add files via upload

---
+++ dist/Hplaylist.js
@@ -0,0 +1,338 @@
+var player = new skPlayer({
+           mode: 'listloop',
+           music: {
+               type: 'file',
+               source: 
+               [
+                   {
+                       name: 'Sanam Re',
+                       author: 'Sanam Re',
+                       src: 'src/1 (1).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Amabalaikum Pombalaikum',
+                       author: 'Kazhugu',
+                       src: 'src/1 (2).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Aval',
+                       author: 'Manithan',
+                       src: 'src/1 (3).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Tere bin',
+                       author: 'Bas Ek Pal',
+                       src: 'src/1 (4).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Hua Hain Aaj Pehli Baar',
+                       author: 'Sanam re',
+                       src: 'src/1 (5).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Socha hai',
+                       author: 'Badhasho',
+                       src: 'src/1 (6).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Main Tera Boyfriend',
+                       author: 'Raabta',
+                       src: 'src/1 (7).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Thean Kudika',
+                       author: 'Maze in Idhayam',
+                       src: 'src/1 (8).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Tu Meri Zindagi Hai',
+                       author: 'Aashiqui',
+                       src: 'src/1 (9).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Tu Jaane Na (Remix)',
+                       author: 'APKGK',
+                       src: 'src/1 (10).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Tera Hone Laga Hoon (Remix)',
+                       author: 'APKGK',
+                       src: 'src/1 (11).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Havana',
+                       author: 'Camila Cabello ft. Young Thug',
+                       src: 'src/1 (12).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Daavuya',
+                       author: 'Remo',
+                       src: 'src/1 (13).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Dandanakka',
+                       author: 'Romeo Juliet',
+                       src: 'src/1 (14).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Liar',
+                       author: 'Dawn Jay',
+                       src: 'src/1 (15).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Dil Tutda',
+                       author: 'Jassi Gill',
+                       src: 'src/1 (16).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Do You Know',
+                       author: 'Diljit Dosanjh',
+                       src: 'src/1 (17).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Donu Donu Donu',
+                       author: 'Maari',
+                       src: 'src/1 (18).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Ek Do Theen',
+                       author: 'Anjaan',
+                       src: 'src/1 (19).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'En Chellame',
+                       author: 'Havoc Mathan',
+                       src: 'src/1 (20).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Endi Ippadi',
+                       author: 'Enakul Oruvan',
+                       src: 'src/1 (21).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Enna Solla',
+                       author: 'Thaangamagan',
+                       src: 'src/1 (22).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Excuse Me Girl',
+                       author: 'Arjun',
+                       src: 'src/1 (23).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Aint Nobody',
+                       author: 'Felix Jaehn',
+                       src: 'src/1 (24).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Hall of fame',
+                       author: 'The Script',
+                       src: 'src/1 (25).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'I Hate Everything About You',
+                       author: '3 Days Grace',
+                       src: 'src/1 (26).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Ijazat Remix',
+                       author: 'Falak',
+                       src: 'src/1 (27).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Judai Kilogram Mix',
+                       author: 'Jannat',
+                       src: 'src/1 (28).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Kaattrae En Vaasal',
+                       author: 'Rhythm',
+                       src: 'src/1 (29).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Kadhali',
+                       author: 'Havoc Mathan',
+                       src: 'src/1 (30).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Kanave Kanave',
+                       author: 'David',
+                       src: 'src/1 (31).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Kannale Kollathe',
+                       author: 'Havoc Mathan',
+                       src: 'src/1 (32).mp3',
+                       cover: 'src/002.jpg'
+                   },{
+                       name: 'Kannazhaga',
+                       author: '3',
+                       src: 'src/1 (33).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Karuthavanlaam Galeejaam',
+                       author: 'Velaikkaran',
+                       src: 'src/1 (34).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Khaab',
+                       author: 'Akhil',
+                       src: 'src/1 (35).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'laree chotee RmX',
+                       author: 'Ek chalis ki last Local',
+                       src: 'src/1 (36).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Local Boys',
+                       author: 'Ethir Neechal',
+                       src: 'src/1 (37).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Oh Oh ',
+                       author: 'Thaangamagan',
+                       src: 'src/1 (38).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Oxygen',
+                       author: 'Kavan',
+                       src: 'src/1 (39).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Paas Aaya Kyun',
+                       author: 'Aggar',
+                       src: 'src/1 (40).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Photo ',
+                       author: 'Karan Shembi',
+                       src: 'src/1 (41).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Poi Solla Intha',
+                       author: 'April Madhatil',
+                       src: 'src/1 (42).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Right By My Side',
+                       author: 'Nicki Minaj',
+                       src: 'src/1 (43).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'So Im Gone',
+                       author: 'Ruchir',
+                       src: 'src/1 (44).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Saajna',
+                       author: 'Falak',
+                       src: 'src/1 (45).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Treat You Better',
+                       author: 'Shawn Mendes',
+                       src: 'src/1 (46).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Stitches',
+                       author: 'Shawn Mendes',
+                       src: 'src/1 (47).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Soniye Hiriye',
+                       author: 'Shael',
+                       src: 'src/1 (48).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Takkaru Takkaru',
+                       author: 'Takkaru Takkaru',
+                       src: 'src/1 (49).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Talking Body RMX',
+                       author: 'Tove Lo',
+                       src: 'src/1 (50).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Tu Hi Haqeeqat',
+                       author: 'Akhil',
+                       src: 'src/1 (51).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Yedhedho Pennae ',
+                       author: 'Ajeesh Harini',
+                       src: 'src/1 (52).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'YembuttuIrukkuthu Aasai',
+                       author: 'D Imman',
+                       src: 'src/1 (53).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Teri Khair Mangdi',
+                       author: 'Bar Bar Dekho',
+                       src: 'src/1 (54).mp3',
+                       cover: 'src/002.jpg'
+                   },
+                   {
+                       name: 'Yennai Maatrum',
+                       author: 'Nanum Rowdydan',
+                       src: 'src/1 (55).mp3',
+                       cover: 'src/002.jpg'
+                   },
+               ]
+           }
+       });
